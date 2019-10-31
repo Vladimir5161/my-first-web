60ec71264df9271f75c1fc9a79f2596582a6dda8
Vladimir <napoleon520@gmail.com>
2019-11-01 00:11:26 +0200

    created media@ for upper header and finished work on adaptivity and page.additional pages and js needs to be created nad written

--- src/app/Header/slider/slides.js
+++ src/app/Header/slider/slides.js
@@ -1,5 +1,6 @@
 const slides = [
   {
+    id: 1,
     image:
       "https://i.pinimg.com/originals/98/57/fa/9857fa2582acee2a3e36ebcdf6ccf3c6.jpg",
     h2: "The Story of Ragnar Lothbrok",
@@ -8,6 +9,7 @@ const slides = [
     button: "Get Started"
   },
   {
+    id: 2,
     image: "https://images3.alphacoders.com/691/thumb-1920-691818.jpg",
     h2: "Vikings invasion",
     h1: "They are coming",
@@ -15,6 +17,7 @@ const slides = [
     button: "Watch on Youtube"
   },
   {
+    id: 3,
     image:
       "https://cdns.kinguin.net/media/category/s/s/ss_97648b570dd8e7061e32b2e623cfed4c5c20fce9.1920x1080_1517652254.jpg",
     h2: "Total war Thrones of britannia",
