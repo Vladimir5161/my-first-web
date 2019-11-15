a9626a4fb9b8898aa360152e676fc7ec2d06c87c
Vladimir <napoleon520@gmail.com>
2019-11-15 17:36:17 +0200

    corrected arrey's ID

--- src/redux/state.js
+++ src/redux/state.js
@@ -3,28 +3,28 @@ let state = {
     season6: {
       contentVideo: [
         {
-          id: "v1",
+          id: 1,
           video: "https://www.youtube.com/watch?v=Y7d0A5re7-0",
           //   image: "/images/conor.jpg",
           name: "Vikings trailer 1",
           description: "season 6"
         },
         {
-          id: "v2",
+          id: 2,
           video: "https://www.youtube.com/watch?v=M7Bu5ed0NvE",
           //   image: "/images/conor.jpg",
           name: "Vikings trailer 1",
           description: "season 6"
         },
         {
-          id: "v3",
+          id: 3,
           video: "https://www.youtube.com/watch?v=IGnoxboek3g",
           //   image: "/images/conor.jpg",
           name: "Vikings trailer 1",
           description: "about Ragnar"
         },
         {
-          id: "v4",
+          id: 4,
           video: "https://www.youtube.com/watch?v=IGnoxboek3g",
           //   image: "/images/conor.jpg",
           name: "Vikings trailer 1",
@@ -33,21 +33,21 @@ let state = {
       ],
       contentStory: [
         {
-          id: "s1",
+          id: 5,
           image: "https://wallpaperaccess.com/full/782445.jpg",
           name: "The Vikings invasion to England",
           story:
             'According to the Anglo-Saxon Chronicles, Viking raiders struck England in 793 and raided Lindisfarne, the monastery that held Saint Cuthbert`s relics. The raiders killed the monks and captured the valuables. The raid marks the beginning of the "Viking Age of Invasion", made possible by the Viking longship. Great but sporadic violence occurred from the last decade of the eighth century on England`s northern and eastern shores; Viking raids continued on a small scale across coastal England. While the initial raiding groups were small, a great amount of planning is believed to have been involved. The Norwegians raided during the winter of 840–841, rather than the usual summer, having waited on an island off Ireland. In 850, Vikings overwintered for the first time in England, on the island of Thanet, Kent. In 854, a raiding party overwintered a second time, at the Isle of Sheppey in the Thames estuary. In 864, they reverted to Thanet for their winter encampment.'
         },
         {
-          id: "s2",
+          id: 6,
           image: "https://wallpaperaccess.com/full/782445.jpg",
           name: "The Vikings invasion to Irland",
           story:
             "The Vikings pillaged monasteries on Ireland`s west coast in 795, and then spread out to cover the rest of the coastline. The north and east of the island were most affected. During the first 40 years, the raids were conducted by small, mobile Viking groups. By 830, the groups consisted of large fleets of Viking ships. From 840, the Vikings began establishing permanent bases at the coasts. Dublin was the most significant settlement in the long term. The Irish became accustomed to the Viking presence. In some cases, they became allies and married each other."
         },
         {
-          id: "s3",
+          id: 7,
           image:
             "https://hdqwalls.com/download/vikings-ragnar-4k-92-1920x1080.jpg",
           name: "The Vikings invasion to Scotland",
@@ -58,38 +58,38 @@ let state = {
       ],
       contentImage: [
         {
-          id: "i1",
+          id: 8,
           image:
             "https://hdqwalls.com/download/vikings-ragnar-4k-92-1920x1080.jpg",
           imageS1:
             "https://torrentfan.net/uploads/posts/2017-01/1485084885_1.jpg"
         },
         {
-          id: "i2",
+          id: 9,
           image: "https://wallpaperaccess.com/full/782445.jpg",
           imageS1:
             "https://miro.medium.com/max/3200/1*nIQpXVuCT6pDQpArpNWGWw.jpeg"
         },
         {
-          id: "i3",
+          id: 10,
           image: "https://wallpaperplay.com/walls/full/5/9/a/55928.jpg",
           imageS1:
             "https://tvfeed.in/cache/40/7f/407f7915b75feb86c4680b175a6f9f18.jpg"
         },
         {
-          id: "i4",
+          id: 11,
           image: "https://wallpaperaccess.com/full/782445.jpg",
           imageS1:
             "http://hahita.com/upload/video/images/big/ab/12/ab12c208c76c483d1056d9a92466139c.jpg"
         },
         {
-          id: "i5",
+          id: 12,
           image: "https://wallpaperplay.com/walls/full/5/9/a/55928.jpg",
           imageS1:
             "https://vikingi.tv/wp-content/uploads/2019/08/2-seria-1-sezon.jpg"
         },
         {
-          id: "i6",
+          id: 13,
           image: "https://wallpaperaccess.com/full/782445.jpg",
           imageS1:
             "http://torrentfilms.ru/wp-content/uploads/2017/04/vikingi-opisanie-serij-1-sezon_1.jpg"
@@ -99,7 +99,7 @@ let state = {
     season1: {
       contentVideo: [
         {
-          id: "v1",
+          id: 14,
           video: "https://www.youtube.com/watch?v=f5av6OqFwz0",
           name: "Vikings season 1 trailer",
           description: "season 1"
@@ -107,7 +107,7 @@ let state = {
       ],
       contentStory: [
         {
-          id: "s1",
+          id: 15,
           image:
             "https://i2.wp.com/www.criticbay.com/wp-content/uploads/2018/11/maxresdefault-6-1024x636-center.jpg?fit=1024%2C636&ssl=1",
           name: "The Vikings Season 1 description",
@@ -117,27 +117,27 @@ let state = {
       ],
       contentImage: [
         {
-          id: "i1",
+          id: 16,
           image:
             "https://cdn-static.denofgeek.com/sites/denofgeek/files/styles/main_wide/public/2018/08/vikings_season_1.jpg?itok=iMCh4zOv"
         },
         {
-          id: "i2",
+          id: 17,
           image:
             "https://vignette.wikia.nocookie.net/bbf6279a-757d-4dd4-b874-cf6b60a6e1e3/scale-to-width-down/800"
         },
         {
-          id: "i3",
+          id: 18,
           image:
             "https://cdn1.thr.com/sites/default/files/imagecache/landscape_928x523/2013/02/vikings_tv_review_a_l.jpg"
         },
         {
-          id: "i4",
+          id: 19,
           image:
             "https://spotlightreport.net/wp-content/uploads/2014/03/vikings-season-1-review.jpg"
         },
         {
-          id: "i5",
+          id: 20,
           image:
             "https://mindreels.files.wordpress.com/2015/11/vikings-season-2-promo.png"
         }
@@ -146,13 +146,13 @@ let state = {
     season2: {
       contentVideo: [
         {
-          id: "v1",
+          id: 21,
           video: "https://www.youtube.com/watch?v=fXOej9DUHJ4",
           name: "Vikings season  wall on wall battle",
           description: "season 2"
         },
         {
-          id: "v2",
+          id: 22,
           video:
             "https://www.youtube.com/watch?v=FDisCTeRQwI&list=PLkGEC13z_M-J1rmnfL9roVmMaelX7aOgU",
           name: "Brother War",
@@ -161,7 +161,7 @@ let state = {
       ],
       contentStory: [
         {
-          id: "s1",
+          id: 23,
           image:
             "https://s1.ibtimes.com/sites/www.ibtimes.com/files/styles/full/public/2014/04/25/vikings-season-2-spoilers.jpeg",
           name: "The Vikings Season 2 description",
@@ -171,21 +171,21 @@ let state = {
       ],
       contentImage: [
         {
-          id: "i1",
+          id: 24,
           image:
             "https://timedotcom.files.wordpress.com/2014/04/fimmel-vikings-cropped.jpg?w=600&quality=85"
         },
         {
-          id: "i2",
+          id: 25,
           image: "https://cdn.wallpapersafari.com/49/44/g4Ifjk.jpg"
         },
         {
-          id: "i3",
+          id: 26,
           image:
             "http://guidagay.info/wp-content/uploads/2018/12/vikings-season-4-part-1-vikings-images-vikings-season-2-episode-1-brothers-war-wallpaper-and-background-photos.jpg"
         },
         {
-          id: "i4",
+          id: 27,
           image:
             "http://www.wallpaperbetter.com/wallpaper/823/866/514/vikings-season-2-2K-wallpaper-middle-size.jpg"
         }
@@ -195,7 +195,7 @@ let state = {
   headerContent: {
     slides: [
       {
-        id: 1,
+        id: 28,
         image:
           "https://i.pinimg.com/originals/98/57/fa/9857fa2582acee2a3e36ebcdf6ccf3c6.jpg",
         h2: "The Story of Ragnar Lothbrok",
@@ -204,7 +204,7 @@ let state = {
         button: "Get Started"
       },
       {
-        id: 2,
+        id: 29,
         image: "https://images3.alphacoders.com/691/thumb-1920-691818.jpg",
         h2: "Vikings invasion",
         h1: "They are coming",
@@ -213,7 +213,7 @@ let state = {
         href: "https://www.youtube.com/watch?v=Y7d0A5re7-0&t=2s"
       },
       {
-        id: 3,
+        id: 30,
         image:
           "https://cdns.kinguin.net/media/category/s/s/ss_97648b570dd8e7061e32b2e623cfed4c5c20fce9.1920x1080_1517652254.jpg",
         h2: "Total war Thrones of britannia",
